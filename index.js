b33cf86238896c8abb450dd554865058c771b939
Maurice Nganga <mauricenganga41@gmail.com>
2021-11-04 09:21:36 +0300

    Add buttons for user to play against machine

--- index.js
+++ index.js
@@ -1,28 +1,54 @@
-let sign = ["Rock", "Paper", "Scissors"];
+let sign = ["rock", "paper", "scissors"];
+let gamePattern = [];
+let playerPattern = [];
 
-let randomNumber1 = Math.floor(Math.random()*3);
-let randomNumber2 = Math.floor(Math.random()*3);
-let playerOneSign = sign[randomNumber1];
-let playerTwoSign = sign[randomNumber2];
-let playerOneShape = "images/shape" + randomNumber1 + ".jpg";
-let playerTwoShape = "images/shape" + randomNumber2 + ".jpg";
+$(".btn").on("click", function(){
+    let playerChosenSign = $(this).attr("id");
+    playerPattern.push(playerChosenSign);
+    let playerShape = "images/" + playerChosenSign + ".jpg";
+    $(".player-img").attr("src", playerShape );
+    playSound(playerChosenSign);
+    nextHand();
+    checkAnswer(playerPattern.length-1);
+})
 
-document.querySelectorAll("img")[0].setAttribute("src", playerOneShape);
-document.querySelectorAll("img")[1].setAttribute("src", playerTwoShape);
 
 
-if(playerOneSign ==="Rock" && playerTwoSign==="Scissors"){
-    $("h1").text("Player one wins!!");
+function checkAnswer(currentSign){
+    if(playerPattern[currentSign] ==="rock" && gamePattern[currentSign] ==="scissors"){
+        $("h1").text("Player one wins!!");
+        
+    }
+    else if(playerPattern[currentSign] ==="paper" && gamePattern[currentSign] === "rock"){
+        $("h1").text("Player one wins!!");
+        
+    }
+    else if(playerPattern[currentSign] === "scissors" && gamePattern[currentSign] === "paper"){
+        $("h1").text("Player one wins!!");
+        
+    }
+    else if(playerPattern[currentSign] === gamePattern[currentSign]){
+        $("h1").text("It's a draw!!");
+        
+    }
+    else{
+        $("h1").text("Player two wins!!");
+        
+    }
 }
-else if(playerOneSign ==="Paper" && playerTwoSign === "Rock"){
-    $("h1").text("Player one wins!!");
+
+function nextHand(){
+    let randomNumber = Math.floor(Math.random()*3);
+    let randomChosenSign = sign[randomNumber];
+    let gameShape = "images/shape" + randomNumber + ".jpg";
+    $(".game-img").attr("src", gameShape);
+    gamePattern.push(randomChosenSign);
+
 }
-else if(playerOneSign === "Scissors" && playerTwoSign === "Paper"){
-    $("h1").text("Player one wins!!");
+
+
+function playSound(name){
+    let audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
 }
-else if(playerOneSign === playerTwoSign){
-    $("h1").text("It's a draw!!");
-}
-else{
-    $("h1").text("Player two wins!!");
-}
+
